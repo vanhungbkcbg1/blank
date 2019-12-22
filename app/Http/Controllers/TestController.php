9dde822782836948857a50bff2dbcc2e084c2c6b
hungnv108 <vanhungbkcbg1@gmail.com>
2019-12-22 16:36:20 +0700

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,7 +7,11 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class TestController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sdfdsfds_dfdsf_dfdsfds(){
+
+    }
 }
